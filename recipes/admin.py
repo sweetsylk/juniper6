6bcacc86de0af254702cae92cc01f6ccfb7d1640
Fatimah Alabdulmuhsin <fatimah.alabdulmuhsin@kcl.ac.uk>
2025-11-16 13:55:13 +0000

    Add initial admin features

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username","email","first_name","last_name","is_active","is_staff")
+    search_fields = ("username","email","first_name","last_name")
+    list_filter =("is_active","is_staff","is_superuser")
+
+    readonly_fields = ("last_login","date_joined")
+
+    ordering = ("last_name","first_name")
+
+    @admin.action(description="Ban selected users")
+    def ban_users(self, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description="Unban selected users")
+    def unban_users(self, request, queryset):
+        queryset.update(is_active=True)
+    
+    actions = ["ban_users","unban_users"]
+
+    fieldsets = (
+    ("Personal Information", {
+        "fields": ("username", "first_name", "last_name", "email")
+    }),
+    ("Permissions", {
+        "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+    }),
+    ("Important Dates", {
+        "fields": ("last_login", "date_joined")
+    }),
+)
+    
+admin.site.site_header = "Juniper-6 Admin Panel"
+admin.site.site_title = "Juniper-6 Backend Admin"
+admin.site.index_title = "Welcome to the Juniper-6 Administration Dashboard"
 
 # Register your models here.
