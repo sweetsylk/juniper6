2b71169404b2171ce3d2b9f96a6fddb5727509c3
Fatimah Alabdulmuhsin <fatimah.alabdulmuhsin@kcl.ac.uk>
2025-11-28 14:14:43 +0000

    Add clickable usernames in recipe cards and enhance profile view test suite

--- recipes/views/profile_view_only.py
+++ recipes/views/profile_view_only.py
@@ -4,19 +4,34 @@ from django.shortcuts import get_object_or_404
 from recipes.models import User, Recipe
 
 class UserProfileView(LoginRequiredMixin, DetailView):
-
+    """
+    Displays a user's profile page.
+    """
     model = User 
     template_name = "profile_view.html"
     context_object_name = "profile_user"
 
     def get_object(self):
+        """
+        Return the User object to display.
+        If the URL contains a username, fetch that user.
+        Otherwise, return the logged-in user.
+        """
         username = self.kwargs.get("username")
         if username is not None: 
+            # Show another user's profile 
             return get_object_or_404(User, username=username)
         
+        # Show the logged-in user's own profile
         return self.request.user
     
     def get_context_data(self, **kwargs):
+        """
+        Add recipe information to the template context:
+        - All recipes by this user
+        - Total recipe count 
+        - Date the user joined 
+        """
         context = super().get_context_data(**kwargs)
         user = self.get_object()
 
