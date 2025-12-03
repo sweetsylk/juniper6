8819384ffc4b23815bf5828b4fb9f52e25dded4e
KENTAH <jeremie.ntumba@kcl.ac.uk>
2025-12-03 17:57:42 +0000

    Introduced a More Comprehensive and "Appealing to Look At" UI - Inspired by Pinterest
    Edited the sidebar, navbar, search bar,  main pages, changed the fonts, figured out the problem with the images

--- recipify/urls.py
+++ recipify/urls.py
@@ -54,6 +54,9 @@ urlpatterns = [
     #tags can have spaces in them, so also need to edit Recipe model to use slugs for them
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
