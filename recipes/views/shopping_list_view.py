8ea5c0915821d70f201788aa904377499d510cf9
Adrian Yip <adrian.yip@kcl.ac.uk>
2025-12-18 09:31:12 +0000

    Add test cases for test_shopping_list_view

--- recipes/views/shopping_list_view.py
+++ recipes/views/shopping_list_view.py
@@ -2,8 +2,9 @@
 
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Sum
+
 
 from recipes.models.recipe import Recipe, RecipeIngredient
 
